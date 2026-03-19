f39a279ca46e69cfb142433dda9b32ebb99cec90
rajumeher1 <minitable2023@gmail.com>
2026-03-19 12:22:15 +0530

    add cache to yml

--- fetchnews/classifier.py
+++ fetchnews/classifier.py
@@ -1,7 +1,6 @@
 # fetchnews/classifier.py
 
 from fetchnews.config import CLIENT
-# import requests
 
 model = "facebook/bart-large-mnli"
 
@@ -16,51 +15,10 @@ def classifier(text):
         multi_label=True,
         model=model
     )
-    # categories = [label for label, score in zip(result["labels"], result["scores"]) if score > 0.5]
-
+    
     categories = [r.label for r in result if r.score > 0.5]
-    # categories = [r["label"] for r in resultif r["score"] > 0.5]
 
     if not categories:
         categories = ['other']
 
-    # print(categories)
-
     return categories
-
-# API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"
-
-# headers = {
-#     "Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"
-# }
-
-# def classifier(text):
-   
-#     payload = {
-#         "inputs": f"News article: {text}",
-#         "parameters": {"candidate_labels": labels, "multi_label": True}
-#     }
-
-#     output = requests.post(API_URL, headers=headers, json=payload, timeout=2)
-
-#     result = output.json()
-
-#     # Extract labels with score > threshold
-#     # Extract labels with score > 0.5
-#     if "labels" in result and "scores" in result:
-#         categories = [
-#             label for label, score in zip(result["labels"], result["scores"])
-#             if score > 0.4
-#         ]
-#     else:
-#         categories = []
-
-#     # Fallback if no labels meet threshold
-#     if not categories:
-#         categories = ['other']
-
-#     return categories
-
-# Example usage
-# text = "Hi, I recently bought a device from your company but it is not working as advertised and I would like to get reimbursed!"
-# print(classifier(text))
